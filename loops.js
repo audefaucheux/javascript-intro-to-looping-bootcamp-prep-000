c55461637ea10efca6e429651318d039e1383387
Aude Faucheux <aude53@gmail.com>
2019-07-02 22:15:24 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -19,11 +19,11 @@ function whileLoop (number) {
 function doWhileLoop(number) {
   var i = 0;
   function incrementVariable() {
-  i = i + 1;
-  return i;
+    i = i + 1;
+    return i;
   }
-    do {
-      console.log("I run once regardless.")
-    } while (incrementVariable() < number)
-  )
+
+  do {
+    console.log("I run once regardless.")
+  } while (incrementVariable() < number)
 }
